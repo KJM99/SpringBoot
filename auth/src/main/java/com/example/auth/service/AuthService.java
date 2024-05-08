899250805c22524c5bb510795e7ec73faa7cd6f5
Kim JaeMin <2098kjm@naver.com>
2024-05-08 11:42:06 +0900

    [feature] function definition

--- auth/src/main/java/com/example/auth/service/AuthService.java
+++ auth/src/main/java/com/example/auth/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.example.auth.service;
 
+import com.example.auth.domain.request.SignInRequest;
 import com.example.auth.domain.request.SignUpRequest;
+import com.example.auth.domain.response.SignInResponse;
 
 public interface AuthService {
-    void insertUser(SignUpRequest signUpRequest);
-
+    void insertUser(SignUpRequest request);
+    SignInResponse signIn(SignInRequest request);
 }
--- auth/src/main/java/com/example/auth/service/AuthServiceImpl.java
+++ auth/src/main/java/com/example/auth/service/AuthServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.auth.service;
 
 import com.example.auth.domain.entity.User;
 import com.example.auth.domain.entity.UserRepository;
+import com.example.auth.domain.request.SignInRequest;
 import com.example.auth.domain.request.SignUpRequest;
+import com.example.auth.domain.response.SignInResponse;
 import jakarta.transaction.Transactional;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -17,14 +19,19 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     @Transactional
-    public void insertUser(SignUpRequest signUpRequest) {
+    public void insertUser(SignUpRequest request) {
         // 1. 유저가 있는지 찾아보고
-        Optional<User> byEmail = userRepository.findByEmail(signUpRequest.email());
+        Optional<User> byEmail = userRepository.findByEmail(request.email());
         // 2-1. 있으면 error
         if(byEmail.isPresent()) throw new IllegalArgumentException();
         // 2-2. 없으면 insert
-        String encodedPassword = passwordEncoder.encode(signUpRequest.password());
-        User user = signUpRequest.toEntity(encodedPassword);
+        String encodedPassword = passwordEncoder.encode(request.password());
+        User user = request.toEntity(encodedPassword);
         userRepository.save(user);
+    }
+
+    @Override
+    public SignInResponse signIn(SignInRequest request) {
+        return null;
     }
 }
